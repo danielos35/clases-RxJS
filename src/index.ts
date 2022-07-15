d3be6a8416354a2ad6a7505f50263e76245b8786
Daniel <studiosdopplermusic@gmail.com>
2022-07-14 19:51:43 -0500

    peticiones

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,47 @@ import { catchError, map } from 'rxjs/operators'
 
 const url = 'https://httpbin.org/delay/1';
 
-
+/*
+ARGUMENTOS GET
+- Argumento #1: Url de la consulta
+- Argumento #2: header
+*/ 
+ajax.get(url,{}); 
 
 /*
-ARGUMENTOS
-- Argumento #1 url a consultar
-- Argumento #2 headers
+ARGUMENTOS POST
+- Argumento #1: Url de la consulta
+- Argumento #2: Body
+- Argumento #3: header
+*/ 
+
+
+ajax.post(url,{
+    id:1
+},{}).subscribe(console.log);
 
 
 
-*/
-const obs$ = ajax.getJSON(url, {
-    'Content-Type':'application/json',
-    'mi-token':'ABC123'
-});
+// OTRA MANERA DE HACER PETICI
+ajax(
+    {
+        url,
+        method:'POST',
+        headers:{}, 
+        body:{
+            id:1
+        }
+    } 
+).subscribe(console.log)
 
-obs$.subscribe(res=>{
-    console.log(res);
-})
+
+ajax(
+    {
+        url,
+        method:'DELETE',
+        headers:{}, 
+        body:{
+            id:1
+        }
+    } 
+).subscribe(console.log)
